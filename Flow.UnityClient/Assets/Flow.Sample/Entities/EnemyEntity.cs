5eafbb96296f3922d42c97ea004dee1a164dea42
김재영 <wodud7308@gmail.com>
2025-11-07 17:45:52 +0900

    setup components

--- Flow.UnityClient/Assets/Flow.Sample/Entities/EnemyEntity.cs
+++ Flow.UnityClient/Assets/Flow.Sample/Entities/EnemyEntity.cs
@@ -3,18 +3,22 @@ using UnityEngine;
 
 namespace Flow.Sample.Entities
 {
-    [RequireComponent(typeof(CombatantComponent))]
+    [RequireComponent(typeof(MoveOnPathComponent), typeof(CombatantComponent), typeof(StatusComponent))]
     public class EnemyEntity : BaseEntity
     {
-        [field:SerializeField] public CombatantComponent Combatant { get; private set; }
-        [field:SerializeField] public StatusComponent Status { get; private set; }
+        [field: SerializeField] public MoveOnPathComponent Move { get; private set; }
+        [field: SerializeField] public CombatantComponent Combatant { get; private set; }
+        [field: SerializeField] public StatusComponent Status { get; private set; }
 
         private void OnValidate()
         {
-            if(Combatant == null)
+            if (Move == null)
+                Move = GetComponent<MoveOnPathComponent>();
+
+            if (Combatant == null)
                 Combatant = GetComponent<CombatantComponent>();
-            
-            if(Status == null)
+
+            if (Status == null)
                 Status = GetComponent<StatusComponent>();
         }
     }
--- Flow.UnityClient/Assets/Flow.Sample/Entities/TowerEntity.cs
+++ Flow.UnityClient/Assets/Flow.Sample/Entities/TowerEntity.cs
@@ -1,7 +1,17 @@
+using Flow.Sample.GamePlay.Components;
+using UnityEngine;
+
 namespace Flow.Sample.Entities
 {
+    [RequireComponent(typeof(CombatantComponent))]
     public class TowerEntity : BaseEntity
     {
-        
+        [field:SerializeField] public CombatantComponent Combatant { get; private set; }
+
+        private void OnValidate()
+        {
+            if(Combatant == null)
+                Combatant = GetComponent<CombatantComponent>();
+        }
     }
 }
